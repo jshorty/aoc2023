7411a095bffa420c5d91afcc1e5ecd2f62c33484
Jake Shorty <jshorty@github.com>
2023-12-01 20:17:58 +0000

    Implement part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,9 +4,24 @@ fn read_lines() -> Vec<String>{
   return fs::read_to_string("input/day1.csv").unwrap().lines().map(String::from).collect();
 }
 
-fn find_calibration_value(line: String) -> i32 {
-  // println!("{}", line);
+fn find_calibration_value(raw_line: String, digits_only: bool) -> i32 {
+  // println!("{}", raw_line);
+  let mut line = raw_line.clone();
+
+  if !digits_only {
+    line = line.replace("one", "o1e")
+               .replace("two", "t2o")
+               .replace("three", "t3e")
+               .replace("four", "4")
+               .replace("five", "5e")
+               .replace("six", "6")
+               .replace("seven", "7n")
+               .replace("eight", "e8t")
+               .replace("nine", "n9e");
+  }
+
   let mut all_digits = String::new();
+
   for c in line.chars() {
     if c.is_digit(10) {
       all_digits.push(c);
@@ -23,11 +38,11 @@ fn find_calibration_value(line: String) -> i32 {
   return result.parse::<i32>().unwrap();
 }
 
-fn read_and_sum_calibration_values() -> i32 {
+fn read_and_sum_calibration_values(digits_only: bool) -> i32 {
   let lines = read_lines();
   let mut sum = 0;
   for line in lines {
-    let val = find_calibration_value(line);
+    let val = find_calibration_value(line, digits_only);
     // println!("{}", val);
     sum += val;
   }
@@ -35,5 +50,6 @@ fn read_and_sum_calibration_values() -> i32 {
 }
 
 pub fn day1() {
-  println!("{}", read_and_sum_calibration_values());
+  println!("Part 1: {}", read_and_sum_calibration_values(true));
+  println!("Part 2: {}", read_and_sum_calibration_values(false));
 }
