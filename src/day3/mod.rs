c5db8ee1a2866b51616d13a18cb2b9ecb5ad99fc
Jake Shorty <jshorty@github.com>
2023-12-03 22:47:17 +0000

    Solve day 3 part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -4,10 +4,7 @@ fn read_lines() -> Vec<String> {
   return fs::read_to_string("input/day3.txt").unwrap().lines().map(String::from).collect();
 }
 
-fn touches_symbol(x: i32, y: i32, symbols: &Vec<(i32, i32)>, print: bool) -> bool {
-  if print {
-    println!("Checking 6! {}, {}", x, y);
-  }
+fn touches_symbol(x: i32, y: i32, symbols: &mut Vec<(i32, i32, char, i32, i32)>, num: i32) -> bool {
   for coords in symbols {
     if x == coords.0 && y == coords.1 {
       return false;
@@ -16,16 +13,22 @@ fn touches_symbol(x: i32, y: i32, symbols: &Vec<(i32, i32)>, print: bool) -> boo
     let dist_x = (x - coords.0).abs();
     let dist_y = (y - coords.1).abs();
     if dist_x < 2 && dist_y < 2 {
+      coords.3 += 1;
+      if coords.4 == 0 {
+        coords.4 = num;
+      } else {
+        coords.4 *= num;
+      }
       return true;
     }
   }
   return false;
 }
 
-fn sum_part_numbers() -> i32 {
+fn sum_part_numbers() -> (i32, Vec<(i32, i32, char, i32, i32)>) {
   let lines = read_lines();
 
-  let mut symbols : Vec<(i32, i32)> = Vec::new();
+  let mut symbols : Vec<(i32, i32, char, i32, i32)> = Vec::new();
   let mut numbers = Vec::new();
   let mut digit_coords = Vec::new();
 
@@ -41,7 +44,7 @@ fn sum_part_numbers() -> i32 {
         }
         current_digit.push_str(&c.to_string());
       } else if c != '.' {
-        symbols.push((x, y));
+        symbols.push((x, y, c, 0, 0));
         if current_digit != "".to_string() {
           numbers.push(current_digit);
           current_digit = "".to_string();
@@ -69,7 +72,7 @@ fn sum_part_numbers() -> i32 {
 
     let mut offset : i32 = 0;
     while offset < length {
-      if touches_symbol(pos.0 + offset, pos.1, &symbols, num == 6) {
+      if touches_symbol(pos.0 + offset, pos.1, &mut symbols, num) {
         touches = true;
         break
       }
@@ -81,10 +84,19 @@ fn sum_part_numbers() -> i32 {
     }
     i += 1;
   }
-  return sum;
+  return (sum, symbols);
 }
 
 pub fn day3() {
-  println!("Part 1: {}", sum_part_numbers());
-  println!("Part 2: {}", 0);
+  let result = sum_part_numbers();
+  println!("Part 1: {}", result.0);
+
+  let symbols = result.1;
+  let mut sum_gear_ratios = 0;
+  for sym in symbols {
+    if sym.2 == '*' && sym.3 == 2 {
+      sum_gear_ratios += sym.4;
+    }
+  }
+  println!("Part 2: {}", sum_gear_ratios);
 }
