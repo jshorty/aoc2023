51fbc71ad402c42e77be88cbf2528273eab9a61c
Jake Shorty <jshorty@github.com>
2023-12-04 05:24:26 +0000

    Solve day 4 part 1

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -4,8 +4,31 @@ fn read_lines() -> Vec<String> {
   return fs::read_to_string("input/day4.txt").unwrap().lines().map(String::from).collect();
 }
 
+fn score_scratchcard(line: String) -> i32 {
+  //println!("Parsing line: {}", line);
+  let sections : Vec<String> = line.split(&[':', '|'][..]).map(String::from).collect();
+  let winning_numbers : Vec<i32> = sections[1].split(' ').filter(|s| s.len() > 0).map(|s| s.parse::<i32>().unwrap()).collect();
+  let card_numbers : Vec<i32> = sections[2].split(' ').filter(|s| s.len() > 0).map(|s| s.parse::<i32>().unwrap()).collect();
+
+  let mut score = 0;
+  for n in winning_numbers {
+    if card_numbers.contains(&n) {
+      if score == 0 {
+        score = 1;
+      } else {
+        score *= 2;
+      }
+    }
+  }
+  return score;
+}
+
 pub fn day4() {
-  read_lines();
-  println!("Part 1: {}", 0);
+  let lines = read_lines();
+  let mut total_score = 0;
+  for line in lines {
+    total_score += score_scratchcard(line);
+  }
+  println!("Part 1: {}", total_score);
   println!("Part 2: {}", 0);
 }
