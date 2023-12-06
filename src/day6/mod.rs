9fe053ed4e81b9a0f2a65b44fe2857c6e94ceeb5
Jake Shorty <jshorty@github.com>
2023-12-06 05:34:06 +0000

    🏌️

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,5 +1,4 @@
 fn victory_options(race: (i64, i64)) -> i64 {
-  println!("Evaluating {} loops", race.0);
   let mut options = 0;
   for s in 1..race.0 {
     if (race.0 - s) * s > race.1 {
@@ -12,14 +11,6 @@ fn victory_options(race: (i64, i64)) -> i64 {
 pub fn day6() {
   let races = [(42, 308), (89, 1170), (91, 1291), (89, 1467)];
   let one_big_race = (42899189, 308117012911467);
-
-  let options = races.map( |r| victory_options(r) );
-  let mut options_product = 1;
-  for options_set in options {
-    options_product *= options_set;
-  }
-
-  let big_race_options = victory_options(one_big_race);
-
-  println!("Part 1: {}\nPart 2: {}", options_product, big_race_options);
+  println!("Part 1: {}", races.map( |r| victory_options(r) ).iter().product::<i64>());
+  println!("Part 2: {}", victory_options(one_big_race));
 }
