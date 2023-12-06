557d1332b0cca6954b196343568623d7312cc0f7
Jake Shorty <jshorty@github.com>
2023-12-06 05:28:47 +0000

    Solve day 6 part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,9 +1,9 @@
-fn victory_options(race: (i32, i32)) -> Vec<i32> {
-  let mut options = Vec::new();
+fn victory_options(race: (i64, i64)) -> i64 {
+  println!("Evaluating {} loops", race.0);
+  let mut options = 0;
   for s in 1..race.0 {
-
     if (race.0 - s) * s > race.1 {
-      options.push(s);
+      options += 1
     }
   }
   return options;
@@ -11,11 +11,15 @@ fn victory_options(race: (i32, i32)) -> Vec<i32> {
 
 pub fn day6() {
   let races = [(42, 308), (89, 1170), (91, 1291), (89, 1467)];
+  let one_big_race = (42899189, 308117012911467);
+
   let options = races.map( |r| victory_options(r) );
-  let mut options_product = 1 as i32;
+  let mut options_product = 1;
   for options_set in options {
-    options_product *= options_set.len() as i32;
+    options_product *= options_set;
   }
 
-  println!("Part 1: {}\nPart 2: {}", options_product, 0);
+  let big_race_options = victory_options(one_big_race);
+
+  println!("Part 1: {}\nPart 2: {}", options_product, big_race_options);
 }
